72878f5ad000c4916ece7affe3a13ed79bd94e26
bomberman <kerjohn.chen@gmail.com>
2018-03-18 18:52:32 -0400

    fixed typo and add sudo

--- jenkins_docker_install.sh
+++ jenkins_docker_install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# this script is only tested on ubuntu xenial
-
 # install docker
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 sudo apt-get update
 sudo apt-get install -y docker-engine
 sudo systemctl enable docker
-sudosystemctl start docker
-sudo usermod -aG docker ubuntu
+sudo systemctl start docker
+sudo usermod -aG docker bomberman
 
 # run jenkins
 sudo mkdir -p /var/jenkins_home
